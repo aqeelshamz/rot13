766862ffd3897c129ac4bc6d35fcb13d833f3e32
aqeel <aqeelten@gmail.com>
2020-11-07 18:32:53 +0530

    first commit

---
+++ lib/rot13.dart
@@ -0,0 +1,70 @@
+library rot13;
+
+Map<String, String> _charset = {
+  "A": "N",
+  "B": "O",
+  "C": "P",
+  "D": "Q",
+  "E": "R",
+  "F": "S",
+  "G": "T",
+  "H": "U",
+  "I": "V",
+  "J": "W",
+  "K": "X",
+  "L": "Y",
+  "M": "Z",
+  "N": "A",
+  "O": "B",
+  "P": "C",
+  "Q": "D",
+  "R": "E",
+  "S": "F",
+  "T": "G",
+  "U": "H",
+  "V": "I",
+  "W": "J",
+  "X": "K",
+  "Y": "L",
+  "Z": "M",
+  "a": "n",
+  "b": "o",
+  "c": "p",
+  "d": "q",
+  "e": "r",
+  "f": "s",
+  "g": "t",
+  "h": "u",
+  "i": "v",
+  "j": "w",
+  "k": "x",
+  "l": "y",
+  "m": "z",
+  "n": "a",
+  "o": "b",
+  "p": "c",
+  "q": "d",
+  "r": "e",
+  "s": "f",
+  "t": "g",
+  "u": "h",
+  "v": "i",
+  "w": "j",
+  "x": "k",
+  "y": "l",
+  "z": "m",
+};
+
+///Encrypt / Decrypt [string] by ROT13
+rot13(String string) {
+  String encrypted = "";
+  for (int i = 0; i < string.length; i++) {
+    if (_charset.keys.contains(string[i])) {
+      encrypted += _charset[string[i]];
+    } else {
+      encrypted += string[i];
+    }
+  }
+
+  return encrypted;
+}
